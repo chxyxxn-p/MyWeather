c62c9c462635c05d024ef1f7ace860b6f0d88b3a
chxyxxn-p <chxyxxn.p@gmail.com>
2020-06-01 18:43:53 +0900

    missing file : previous commit (set HomePage layout)

--- src/home/HomePage.java
+++ src/home/HomePage.java
@@ -33,53 +33,50 @@ public class HomePage extends Page {
 	JPanel nowImgPn;
 	JTextArea nowInfoTa;
 	
+	JPanel recommendPanel;
+	
+	JPanel diaryPanel;
+	
+	JPanel todoListPanel;
+	
+	
 	public HomePage(MainDrive mainDrive, String title, int width, int height, boolean showFlag) {
 		
 		super(mainDrive, title, width, height, showFlag);
-		
+
+//		nowWeather
 		WeatherPage wp = ((WeatherPage)mainDrive.getPages()[1]);
 		
 		nowPanel = new JPanel();
 		nowImgPn = new JPanel() {
 			@Override
 			public void paint(Graphics g) {
-				g.drawImage(nowImg, 0, 0, width/3-40, width/3-40, mainDrive);
+				g.drawImage(nowImg = wp.getNowImg(), 0, 0, width/4-40, width/4-40, mainDrive);
 			}
 		};
 		nowInfoTa = new JTextArea();
+		
 		nowInfoTa.setEditable(false);
 		
-		WeatherValue nwv =  wp.getNwv();
-		
-		nowImg = wp.getNowImg();
-
 		nowInfoTa.setText(wp.getNowInfoTaText());
 
-//		nowInfoTa.setAlignmentX(JTextArea.CENTER_ALIGNMENT);
-//		nowInfoTa.setAlignmentY(JTextArea.CENTER_ALIGNMENT);
-
-		nowInfoTa.setFont(mainDrive.getFont(25));
+		nowInfoTa.setFont(mainDrive.getFont(20));
 		
+//		nowPanel.setBackground(Color.pink);
+		nowPanel.setBackground(new Color(0,0,0,0));
 		nowImgPn.setBackground(new Color(0,0,0,0));
 		nowInfoTa.setBackground(new Color(0,0,0,0));
 		nowInfoTa.setOpaque(true);
-		nowPanel.setBackground(new Color(0,0,0,0));
 		
 		nowPanel.setLayout(null);
 		
-		nowImgPn.setBounds(20, 10, width/3-40, width/3-40);
-		nowInfoTa.setBounds(10, 10+width/3-40+10, width/3-20, height - width/3 +40 - 30);
+		nowImgPn.setBounds(20, 10, width/4-40, width/4-40);
+		nowInfoTa.setBounds(10, 10+width/4-40+10, width/4-20, height - width/4 +40 - 30);
 		
 		nowPanel.add(nowImgPn);
 		nowPanel.add(nowInfoTa);
-		
-		nowPanel.setBounds(0, 0, width/3, height);
-		
-
-		this.setLayout(null);
-		this.add(nowPanel);
-		
-		nowImgPn.addMouseListener(new MouseAdapter() {
+			
+		nowPanel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				mainDrive.changePage(1);
@@ -94,6 +91,61 @@ public class HomePage extends Page {
 				nowPanel.repaint();
 			}
 		});
-				
+			
+		
+//		recommend
+		recommendPanel = new JPanel();
+		
+		recommendPanel.setBackground(Color.yellow);
+//		recommendPanel.setBackground(new Color(0,0,0,0));
+
+		recommendPanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				mainDrive.changePage(4);
+			}
+		});
+		
+		
+//		diary
+		diaryPanel = new JPanel();
+		
+		diaryPanel.setBackground(Color.green);		
+//		diaryPanel.setBackground(new Color(0,0,0,0));
+		
+		diaryPanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				mainDrive.changePage(2);
+			}
+		});
+
+		
+//		toDoList
+		todoListPanel = new JPanel();
+		
+		todoListPanel.setBackground(Color.blue);		
+//		todoListPanel.setBackground(new Color(0,0,0,0));
+
+		todoListPanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				mainDrive.changePage(2);
+			}
+		});
+		
+		
+//		page
+		this.setLayout(null);
+		
+		nowPanel.setBounds(0, 0, width/4, height/3*2);
+		diaryPanel.setBounds(width/4 + 10, 0, (width - width/4 + 10 + 10)/2, height/3*2);
+		todoListPanel.setBounds((width/4 + 10) + ((width - width/4 + 10 + 10)/2 + 10), 0, (width - width/4 + 10 + 10)/2, height/3*2);
+		recommendPanel.setBounds(0, height/3*2+10, width, height - (height/3*2+10));
+		
+		this.add(nowPanel);
+		this.add(diaryPanel);
+		this.add(todoListPanel);
+		this.add(recommendPanel);
 	}
 }
