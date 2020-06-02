ffd696be08a0305c1f57d1464d5c76ab8a45037b
chxyxxn-p <chxyxxn.p@gmail.com>
2020-06-02 16:00:49 +0900

    - set locationPanel of HomePage layout
    - create variable(width, height) about panels in HomePage

--- src/home/HomePage.java
+++ src/home/HomePage.java
@@ -2,24 +2,21 @@ package home;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.event.AdjustmentEvent;
-import java.awt.event.AdjustmentListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Map;
 
-import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import main.MainDrive;
 import main.Page;
-import weather.FcstPanel;
 import weather.WeatherPage;
 import weather.WeatherValue;
 
@@ -34,6 +31,10 @@ public class HomePage extends Page {
 	JTextArea nowInfoTa;
 	
 	JPanel locationPanel;
+	JComboBox<String> sepFristCb;
+	JComboBox<String> sepSecondCb;
+	JComboBox<String> sepThirdCb;
+	JButton locationBt;
 	
 	JPanel recommendPanel;
 	
@@ -42,11 +43,32 @@ public class HomePage extends Page {
 	JPanel todoListPanel;
 	
 	
+	int nowPanelWidth;
+	int nowPanelHeight;
+	int locationPanelWidth;
+	int locationPanelHeight;
+	int diaryPanelWidth;
+	int diaryPanelHeight;
+	int todoListPanelWidth;
+	int todoListPanelHeight;
+	int recommendPanelWidth;
+	int recommendPanelHeight;
+	
+	
 	public HomePage(MainDrive mainDrive, String title, int width, int height, boolean showFlag) {
 		
 		super(mainDrive, title, width, height, showFlag);
+		
+		 nowPanelWidth = width/4;
+		 nowPanelHeight = height/3*2-5;
+		 locationPanelWidth = width - (nowPanelWidth + 10);
+		 locationPanelHeight = nowPanelHeight/7;
+		 todoListPanelWidth = diaryPanelWidth = (width-nowPanelWidth-10)/2;
+		 todoListPanelHeight = diaryPanelHeight = nowPanelHeight - locationPanelHeight - 10;
+		 recommendPanelWidth = width;
+		 recommendPanelHeight = height/3-5;
 
-//		nowWeather
+//		[nowWeather]
 		WeatherPage wp = ((WeatherPage)mainDrive.getPages()[1]);
 		
 		nowPanel = new JPanel();
@@ -62,10 +84,10 @@ public class HomePage extends Page {
 		
 		nowInfoTa.setText(wp.getNowInfoTaText());
 
-		nowInfoTa.setFont(mainDrive.getFont(20));
+		nowInfoTa.setFont(mainDrive.getFont(18));
 		
-//		nowPanel.setBackground(Color.pink);
-		nowPanel.setBackground(new Color(0,0,0,0));
+		nowPanel.setBackground(Color.cyan);
+//		nowPanel.setBackground(new Color(0,0,0,0));
 		nowImgPn.setBackground(new Color(0,0,0,0));
 		nowInfoTa.setBackground(new Color(0,0,0,0));
 		nowInfoTa.setOpaque(true);
@@ -95,11 +117,36 @@ public class HomePage extends Page {
 		});
 		
 		
-//		location
+//		[location]
 		locationPanel = new JPanel();
+		sepFristCb = new JComboBox<String>();
+		sepSecondCb = new JComboBox<String>();
+		sepThirdCb = new JComboBox<String>();
+		locationBt = new JButton("search");
+		
+		sepFristCb.setFont(mainDrive.getFont(12));
+		sepSecondCb.setFont(mainDrive.getFont(12));
+		sepThirdCb.setFont(mainDrive.getFont(12));
+		locationBt.setFont(mainDrive.getFont(10));
 		
 		locationPanel.setBackground(Color.yellow);
 //		locationPanel.setBackground(new Color(0,0,0,0));
+		sepFristCb.setBackground(Color.white);
+		sepSecondCb.setBackground(Color.white);
+		sepThirdCb.setBackground(Color.white);
+		locationBt.setBackground(Color.white);
+		
+		locationPanel.setLayout(null);
+		
+		sepFristCb.setBounds(10, 10, (locationPanelWidth-50)/10*3, locationPanelHeight-20);
+		sepSecondCb.setBounds(20 + (locationPanelWidth-50)/10*3, 10, (locationPanelWidth-50)/10*3, locationPanelHeight-20);
+		sepThirdCb.setBounds(30 + (locationPanelWidth-50)/10*6, 10, (locationPanelWidth-50)/10*3, locationPanelHeight-20);
+		locationBt.setBounds(40 + (locationPanelWidth-50)/10*9, 10, (locationPanelWidth-50)/10*1, locationPanelHeight-20);
+		
+		locationPanel.add(sepFristCb);
+		locationPanel.add(sepSecondCb);
+		locationPanel.add(sepThirdCb);
+		locationPanel.add(locationBt);
 
 		locationPanel.addMouseListener(new MouseAdapter() {
 			@Override
@@ -108,7 +155,17 @@ public class HomePage extends Page {
 			}
 		});
 		
-//		recommend
+		locationBt.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+//				user가 고른 위치에 해당하는 nx, ny가져와서 mainDrive의 searchNx, searchNy로 대입하고, 스레드로 새로 데이터 불러오기
+				System.out.println("click");
+			}
+		});
+		
+		
+//		[recommend]
 		recommendPanel = new JPanel();
 		
 		recommendPanel.setBackground(Color.magenta);
@@ -122,7 +179,7 @@ public class HomePage extends Page {
 		});
 		
 		
-//		diary
+//		[diary]
 		diaryPanel = new JPanel();
 		
 		diaryPanel.setBackground(Color.green);		
@@ -136,7 +193,7 @@ public class HomePage extends Page {
 		});
 
 		
-//		toDoList
+//		[toDoList]
 		todoListPanel = new JPanel();
 		
 		todoListPanel.setBackground(Color.blue);		
@@ -150,14 +207,14 @@ public class HomePage extends Page {
 		});
 		
 		
-//		page
+//		[HomePage]
 		this.setLayout(null);
 		
-		nowPanel.setBounds(0, 0, width/4, height/3*2+20);
-		locationPanel.setBounds(width/4 + 10, 0, width - (width/4+10), 50);
-		diaryPanel.setBounds(width/4 + 10, 60, (width - width/4 + 10 + 10)/2, height/3*2-40);
-		todoListPanel.setBounds((width/4 + 10) + ((width - width/4 + 10 + 10)/2 + 10), 60, (width - width/4 + 10 + 10)/2, height/3*2-40);
-		recommendPanel.setBounds(0, height/3*2+30, width, height - (height/3*2+10)-20);
+		nowPanel.setBounds(0, 0, nowPanelWidth, nowPanelHeight);
+		locationPanel.setBounds(nowPanelWidth + 10, 0, locationPanelWidth, locationPanelHeight);
+		diaryPanel.setBounds(nowPanelWidth + 10, locationPanelHeight + 10, diaryPanelWidth, diaryPanelHeight);
+		todoListPanel.setBounds((nowPanelWidth + 10) + (diaryPanelWidth + 10), locationPanelHeight + 10, todoListPanelWidth, todoListPanelHeight);
+		recommendPanel.setBounds(0, nowPanelHeight + 10, recommendPanelWidth, recommendPanelHeight);
 		
 		this.add(nowPanel);
 		this.add(locationPanel);
